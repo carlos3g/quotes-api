61b2232ab4ae3683978e6e7d9f52daa4dd4bc4de
Carlos Mesquita <52337966+carlos3g@users.noreply.github.com>
2023-01-08 12:14:22 -0300

    fix: fixes filesystem paths wrong matches in build

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,12 +1,13 @@
+import path from 'path';
 import { DataSource, DataSourceOptions } from 'typeorm';
 import { SeederOptions } from 'typeorm-extension';
 
 const dataSourceOptions: DataSourceOptions & SeederOptions = {
   type: 'sqlite',
-  database: './dist/database/database.sqlite',
-  entities: ['./dist/entities/*.entity.js'],
-  migrations: ['./dist/database/migrations/*.js'],
-  seeds: ['./dist/database/seeds/*.seed.js'],
+  database: path.join(__dirname, 'database.sqlite'),
+  entities: [path.join(__dirname, '../entities/*.entity.?s')],
+  migrations: [path.join(__dirname, 'migrations/*.?s')],
+  seeds: [path.join(__dirname, 'seeds/*.seed.?s')],
 };
 
 const dataSource = new DataSource(dataSourceOptions as DataSourceOptions);
